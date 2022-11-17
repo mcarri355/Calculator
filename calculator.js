74fde57c410fa08a0e897128151ca326fc88402d
mcarri355 <carrillomatisa@gmail.com>
2022-11-16 18:21:57 -0700

    js calculator update

--- calculator.js
+++ calculator.js
@@ -1,46 +1,61 @@
 function b0(){
+    document.getElementById("input").value = "0";
 }
 
 function b1(){
+    document.getElementById("input").value = "1";
 }
 
 function b2(){
+    document.getElementById("input").value = "2";
 }
 
 function b3(){
+    document.getElementById("input").value = "3";
 }
 
 function b4(){
+    document.getElementById("input").value = "4";
 }
 
 function b5(){
+    document.getElementById("input").value = "5";
 }
 
 function b6(){
+    document.getElementById("input").value = "6";
 }
 
 function b7(){
+    document.getElementById("input").value = "7";
 }
 
 function b8(){
+    document.getElementById("input").value = "8";
 }
 
 function b9(){
+    document.getElementById("input").value = "9";
 }
 
 function decimal() {
+    document.getElementById("input").value = ".";
 }
 
 function add(){
+    document.getElementById("input").value = "+";
 }
 
 function subtract(){
+    document.getElementById("input").value = "-";
 }
 
 function divide(){
+    document.getElementById("input").value = "/";
 }
 
 function multiply(){
+    document.getElementById("input").value = "*";
 }
 
 function equal() {
