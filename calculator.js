b66d4706c942177fc6d1e3a72d839ba3877d1a4f
mcarri355 <carrillomatisa@gmail.com>
2022-11-16 17:16:05 -0700

    css and html updates

---
+++ calculator.js
@@ -0,0 +1,48 @@
+function b0(){
+}
+
+function b1(){
+}
+
+function b2(){
+}
+
+function b3(){
+}
+
+function b4(){
+}
+
+function b5(){
+}
+
+function b6(){
+}
+
+function b7(){
+}
+
+function b8(){
+}
+
+function b9(){
+}
+
+function decimal() {
+}
+
+function add(){
+}
+
+function subtract(){
+}
+
+function divide(){
+}
+
+function multiply(){
+}
+
+function equal() {
+}
+
